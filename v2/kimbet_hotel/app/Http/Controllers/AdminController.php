924ef86238b23b3ec3c7acc71fde9bc7b4556fed
Mohamad Helmi Mohamad Sayono <mohdh6298@gmail.com>
2025-01-23 01:35:28 +0800

    Display data on admin pane

--- v2/kimbet_hotel/app/Http/Controllers/AdminController.php
+++ v2/kimbet_hotel/app/Http/Controllers/AdminController.php
@@ -50,4 +50,9 @@ class AdminController extends Controller{
 
         return redirect()->back();
     }
+
+    public function view_room(){
+        $data = Room::all();
+        return view('admin.view_room', compact('data'));
+    }
 }
