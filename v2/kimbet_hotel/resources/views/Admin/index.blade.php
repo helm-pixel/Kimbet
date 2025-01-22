93ea201db5f9925d1f7dd650a883e5988819f057
Mohamad Helmi Mohamad Sayono <mohdh6298@gmail.com>
2025-01-22 12:56:44 +0800

    complete multiple auth

--- v2/kimbet_hotel/resources/views/Admin/index.blade.php
+++ v2/kimbet_hotel/resources/views/Admin/index.blade.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Dashboard</title>
 </head>
 <body>
-    <h1>Admin Home</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
+    
+</x-app-layout>
 </body>
 </html>
