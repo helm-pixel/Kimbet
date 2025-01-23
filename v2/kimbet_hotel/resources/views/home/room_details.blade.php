8c0e8da34cbffaac1d0eb84b5ce37fed575b10ef
Mohamad Helmi Mohamad Sayono <mohdh6298@gmail.com>
2025-01-23 11:09:48 +0800

    Add booking room functionality

--- v2/kimbet_hotel/resources/views/home/room_details.blade.php
+++ v2/kimbet_hotel/resources/views/home/room_details.blade.php
@@ -3,6 +3,18 @@
    <head>
     <base href="/public">
       @include('home.css')
+      <style>
+         label{
+            display: inline-block;
+            width: 200px;
+         }
+
+         input{
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+         }
+      </style>
    </head>
    <!-- body -->
    <body class="main-layout">
@@ -28,7 +40,7 @@
             </div>
             <div class="row">
                <div class="col-md-8">
-                  <div id="serv_hover"  class="room">
+                  <div id="serv_hover" class="room">
                      <div class="room_img" style="padding: 20px">
                         <img style="height: 300px; 800px" src="/room/{{$room->image}}" alt="#"/>
                      </div>
@@ -48,9 +60,53 @@
                         <h3 style="padding: 12px">
                            Price: {{$room->price}}
                         </h3>
-                        
                      </div>
                   </div>
+               </div>
+               <div class="col-md-4">
+                  <h1 style="font-size: 40px!important;">Book Room</h1>
+                  @if($errors)
+                  @foreach($errors->all() as $errors)
+                  <li style="color: red;">
+                     {{$errors}}
+                  </li>
+                  @endforeach
+                  @endif
+                  <form action="{{url('add_booking', $room->id)}}" method="Post">
+                  @csrf
+                  <div>
+                     <label>Name</label>
+                     <input type="text" name="name" 
+                     @if(Auth::id())
+                     value="{{Auth::user()->name}}"
+                     @endif>
+                  </div>
+                  <div>
+                     <label>Email</label>
+                     <input type="email" name="email"
+                     @if(Auth::id())
+                     value="{{Auth::user()->email}}"
+                     @endif>
+                  </div>
+                  <div>
+                     <label>Phone</label>
+                     <input type="number" name="phone"
+                     @if(Auth::id())
+                     value="{{Auth::user()->phone}}"
+                     @endif>
+                  </div>
+                  <div>
+                     <label>Start Date</label>
+                     <input type="date" name="startDate"8 id="startDate">
+                  </div>
+                  <div>
+                     <label>End Date</label>
+                     <input type="date" name="endDate" id="endDate">
+                  </div>
+                  <div style="padding-top: 10px">
+                     <input type="submit" value="Book Now" class="btn btn-primary">
+                  </div>
+                  </form>
                </div>
             </div>
          </div>
@@ -58,5 +114,24 @@
 
       <!--  footer -->
       @include('home.footer')
+
+      <script>
+         $(function(){
+            var dtToday = new Date();
+            var month = dtToday.getMonth() + 1;
+            var day = dtToday.getDate();
+            var year = dtToday.getFullYear();
+
+            if(month < 10)
+               month = '0' + month.toString();
+
+            if(day < 10)
+               day = '0' + day.toString();
+
+            var maxDate = year + '-' + month + '-' + day;
+            $('#startDate').attr('min', maxDate);
+            $('#endDate').attr('min', maxDate);
+         });
+      </script>
    </body>
 </html>
